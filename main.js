7169bba4e97744ae767e529736dba4327a9d328d
Your Nameğ <dr.hakanonay@gmail.com>
2020-04-28 11:36:58 +0200

    hakan-clean

---
+++ main.js
@@ -0,0 +1,47 @@
+
+
+nav {
+
+    width: 100%;
+    height:70px;
+    position: absolute;
+    padding: 0px 18px 5px;
+    margin-top: 0%;
+    position: absolute;
+    background-color: #e9f7f4;
+    color:#13287e;
+
+}
+.h2 {
+
+    float: left;
+    position: absolute;
+    margin-left: 10px;
+    font: x-large;
+    font-size: 18px;
+    columns: #5f5f5f;;
+    clear: both;
+    padding: 0;
+    vertical-align: baseline;
+}
+
+.classa { 
+
+    float: right;
+    font: xx-large;
+    font-weight: 600;
+    position: absolute;
+    margin-left: 14px;
+    font-size: large;
+    margin-top: 30px;    
+    color: #13287e;
+    padding: 0px 18px 5px;
+}
+body {
+    width: 100%;
+    height: 600px;
+    background-image: url(cleanindex.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+
+}
